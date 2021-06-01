a4c6239db85a84178094debabfde144a44b97280
hui <fwu@today36524.com>
2021-06-01 14:15:40 +0800

    add 你能在你最喜欢的那天吃到你最喜欢的糖果吗？(中等 前缀和)

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -225,7 +225,10 @@ public class NacosPassword {
 
 //        int[] a = {3, 4, 5, 2, 1, 7, 3, 4, 7};
 //        System.out.println(minChanges(a, 3));
-        System.out.println(reverseParentheses("(ed(et(oc))el)"));
+//        System.out.println(reverseParentheses("(ed(et(oc))el)"));
+        int[] a = {5, 2, 6, 4, 1};
+        int[][] b = {{3, 1, 2}, {4, 10, 3}, {3, 10, 100}, {4, 100, 30}, {1, 3, 1}};
+        System.out.println(canEat(a, b));
     }
 
 
@@ -239,14 +242,49 @@ public class NacosPassword {
         }
     }
 
-     // 4的幂
+    //你能在你最喜欢的那天吃到你最喜欢的糖果吗？
+//    给你一个下标从 0 开始的正整数数组 candiesCount ，其中 candiesCount[i] 表示你拥有的第 i 类糖果的数目。同时给你一个二维数组 queries ，其中 queries[i] = [favoriteTypei, favoriteDayi, dailyCapi] 。
+//
+//    你按照如下规则进行一场游戏：
+//
+//    你从第 0 天开始吃糖果。
+//    你在吃完 所有 第 i - 1 类糖果之前，不能 吃任何一颗第 i 类糖果。
+//    在吃完所有糖果之前，你必须每天 至少 吃 一颗 糖果。
+//
+//    请你构建一个布尔型数组 answer ，满足 answer.length == queries.length 。answer[i] 为 true 的条件是：在每天吃 不超过 dailyCapi 颗糖果的前提下，你可以在第 favoriteDayi 天吃到第 favoriteTypei 类糖果；否则 answer[i] 为 false 。注意，只要满足上面 3 条规则中的第二条规则，你就可以在同一天吃不同类型的糖果。
+//
+//    请你返回得到的数组 answer 。
+    public static boolean[] canEat(int[] candiesCount, int[][] queries) {
+        int n = candiesCount.length;
+        int m = queries.length;
+        boolean[] res = new boolean[m];
+        long[] sum = new long[n];
+        sum[0] = candiesCount[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + candiesCount[i];
+        }
+        for (int i = 0; i < m; i++) {
+            int favoriteType = queries[i][0];
+            int favoriteDay = queries[i][1];
+            int dailyCap = queries[i][2];
+
+            long minCount = favoriteDay + 1;
+            long maxCount = (long)(favoriteDay + 1) * dailyCap;
+            long minTyPeCount = favoriteType == 0 ? 1 : sum[favoriteType - 1] + 1;
+            long maxTypeCount = sum[favoriteType];
+            res[i] = !(minCount > maxTypeCount || maxCount < minTyPeCount);
+        }
+        return res;
+    }
+
+    // 4的幂
 //    给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
 //
 //    整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
     public static boolean isPowerOfFour(int n) {
         //n & (n - 1)去掉二进制末尾的1 等于0，判断是不是2的幂次方
         // n & 0x55555555 ，4的幂次方只有一个1且在奇数位上
-        return n > 0 && (n & (n - 1)) ==0 && (n & 0x55555555) != 0;
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 
 //    汉明距离总和
