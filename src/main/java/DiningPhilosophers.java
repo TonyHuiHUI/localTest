95a58404dd0fa96d9cb3acce6c08dd305422bd20
hui <fwu@today36524.com>
2021-06-02 18:03:28 +0800

    add 哲学家进餐（中等 多线程 Semaphore ReetrantLock）

---
+++ src/main/java/DiningPhilosophers.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DiningPhilosophers {
+    //5把叉子，5把锁
+    ReentrantLock[] reentrantLocks = {
+            new ReentrantLock(),
+            new ReentrantLock(),
+            new ReentrantLock(),
+            new ReentrantLock(),
+            new ReentrantLock(),
+    };
+    //最多允许4名哲学家进餐，防止死锁
+    Semaphore semaphore = new Semaphore(4);
+
+    public DiningPhilosophers() {
+
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+        int rightFork = philosopher;
+        int leftFork = (philosopher + 1) % 5;
+        semaphore.acquire();
+        reentrantLocks[rightFork].lock();
+        reentrantLocks[leftFork].lock();
+        pickLeftFork.run();
+        pickRightFork.run();
+        eat.run();
+        putLeftFork.run();
+        putRightFork.run();
+        reentrantLocks[rightFork].unlock();
+        reentrantLocks[leftFork].unlock();
+    }
+}
