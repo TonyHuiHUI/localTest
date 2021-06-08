72fe4243746fd64df86ff195677076228c1b0301
hui <fwu@today36524.com>
2021-06-08 17:57:54 +0800

    add  最后一块石头的重量 II(中等 动态规划（0-1）)

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -232,8 +232,10 @@ public class NacosPassword {
 //        int[] a = {1, 1, 1, 1, 1};
 //        System.out.println(findMaxLength(a));
 //        System.out.println(findTargetSumWays(a, 3));
-        int[] a = {2,4};
-        System.out.println(largestRectangleArea(a));
+//        int[] a = {2,4};
+//        System.out.println(largestRectangleArea(a));
+        int[] a ={2,7,4,1,8,1};
+        System.out.println(lastStoneWeightII(a));
     }
 
 
@@ -264,6 +266,34 @@ public class NacosPassword {
         }
     }
 
+    //1049. 最后一块石头的重量 II
+//    有一堆石头，用整数数组 stones 表示。其中 stones[i] 表示第 i 块石头的重量。
+//
+//    每一回合，从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+//
+//    如果 x == y，那么两块石头都会被完全粉碎；
+//    如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+//
+//    最后，最多只会剩下一块 石头。返回此石头 最小的可能重量 。如果没有石头剩下，就返回 0。
+    public static int lastStoneWeightII(int[] stones) {
+        int n = stones.length;
+        int sum = 0;
+        for (int i : stones) {
+            sum += i;
+        }
+        int t = sum / 2;
+        int[][] f = new int[n + 1][t + 1];
+        for (int i = 1; i <= n; i++) {
+            int x = stones[i - 1];
+            for (int j = 0; j <= t; j++) {
+                f[i][j] = f[i - 1][j];
+                if (j >= x) {
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j - x] + x);
+                }
+            }
+        }
+        return Math.abs(sum - f[n][t] - f[n][t]);
+    }
     //84. 柱状图中最大的矩形
 //    给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
 //    求在该柱状图中，能够勾勒出来的矩形的最大面积。
