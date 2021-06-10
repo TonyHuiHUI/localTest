ed230032394a0f70ff95d9fd266e822b0ec77afb
hui <fwu@today36524.com>
2021-06-10 18:06:18 +0800

    add 零钱兑换ii（中等 动态规划（完全背包问题））

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -269,9 +269,20 @@ public class NacosPassword {
 
     //518. 零钱兑换 II
     //给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
-    public static int change(int amount, int[] coins) {
-
-        return 1;
+    public static int change(int cnt, int[] cs) {
+        int n = cs.length;
+        int[][] f = new int[n + 1][cnt + 1];
+        f[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int val = cs[i - 1];
+            for (int j = 0; j <= cnt; j++) {
+                f[i][j] = f[i - 1][j];
+                for (int k = 1; k * val <= j; k++) {
+                    f[i][j] += f[i - 1][j - k * val];
+                }
+            }
+        }
+        return f[n][cnt];
     }
 
     //322. 零钱兑换
