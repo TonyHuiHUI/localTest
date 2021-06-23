0d954a764560fd601876d58aa6cce0efa23ca534
hui <fwu@today36524.com>
2021-06-23 11:26:11 +0800

    add 二进制1的个数

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -272,6 +272,16 @@ public class NacosPassword {
             this.next = next;
         }
     }
+//    剑指 Offer 15. 二进制中1的个数
+//    请实现一个函数，输入一个整数（以二进制串形式），输出该数二进制表示中 1 的个数。例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n != 0){
+            n = n & (n -1);
+            count++;
+        }
+        return count;
+    }
     //剑指 Offer 38. 字符串的排列
     //    输入一个字符串，打印出该字符串中字符的所有排列。
     //    你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
