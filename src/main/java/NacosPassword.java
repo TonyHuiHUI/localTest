a9714f62b88b008a5b13df65bc92448f6c39b32b
hui <fwu@today36524.com>
2021-06-28 17:57:05 +0800

    add 公交线路（困难 建图 + 广度优先搜索）

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -243,8 +243,10 @@ public class NacosPassword {
 //        System.out.println(stoneGame(a));
 //        System.out.println(smallestGoodBase("4681"));
 //        System.out.println(permutation("abc"));
-        String[] a = {"0201","0101","0102","1212","2002"};
-        System.out.println(openLock(a,"0202"));
+//        String[] a = {"0201", "0101", "0102", "1212", "2002"};
+//        System.out.println(openLock(a, "0202"));
+        int[][] a = {{1, 2, 7}, {3, 6, 7}};
+        System.out.println(numBusesToDestination(a, 1, 6));
     }
 
 
@@ -273,6 +275,57 @@ public class NacosPassword {
             this.val = val;
             this.next = next;
         }
+    }
+
+    //815. 公交路线
+//    给你一个数组 routes ，表示一系列公交线路，其中每个 routes[i] 表示一条公交线路，第 i 辆公交车将会在上面循环行驶。
+//    例如，路线 routes[0] = [1, 5, 7] 表示第 0 辆公交车会一直按序列 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... 这样的车站路线行驶。
+//    现在从 source 车站出发（初始时不在公交车上），要前往 target 车站。 期间仅可乘坐公交车。
+//    求出 最少乘坐的公交车数量 。如果不可能到达终点车站，返回 -1 。
+    public static int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+        int n = routes.length;
+        //记录公交线路是否相通
+        boolean[][] edge = new boolean[n][n];
+        //记录车站对应的路线
+        Map<Integer, List<Integer>> rec = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            for (int site : routes[i]) {
+                List<Integer> list = rec.getOrDefault(site, new ArrayList<Integer>());
+                for (int j : list) {
+                    edge[i][j] = edge[j][i] = true;
+                }
+                list.add(i);
+                rec.put(site, list);
+            }
+        }
+
+        int[] dis = new int[n];
+        Arrays.fill(dis, -1);
+        Queue<Integer> que = new LinkedList<Integer>();
+        for (int bus : rec.getOrDefault(source, new ArrayList<Integer>())) {
+            dis[bus] = 1;
+            que.offer(bus);
+        }
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            for (int y = 0; y < n; y++) {
+                if (edge[x][y] && dis[y] == -1) {
+                    dis[y] = dis[x] + 1;
+                    que.offer(y);
+                }
+            }
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int bus : rec.getOrDefault(target, new ArrayList<Integer>())) {
+            if (dis[bus] != -1) {
+                ret = Math.min(ret, dis[bus]);
+            }
+        }
+        return ret == Integer.MAX_VALUE ? -1 : ret;
     }
 
     //752. 打开转盘锁
