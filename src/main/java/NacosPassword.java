ccc48b575ec7bc30803f85bacf1a6feda7640a65
hui <fwu@today36524.com>
2021-07-07 17:40:13 +0800

    add 无重复字符的最长子串（中等 set 滑动窗口）

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -290,23 +290,29 @@ public class NacosPassword {
             this.next = next;
         }
     }
+
     //3. 无重复字符的最长子串
+    //给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
     public static int lengthOfLongestSubstring(String s) {
+        //集合去重
         Set<Character> set = new HashSet<>();
         int index = 0;
         int result = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(i != 0){
-                set.remove(s.charAt(i-1));
+        for (int i = 0; i < s.length(); i++) {
+            //字符串重复，左移边界，从set移除一个字符
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
             }
-            while (index < s.length() && !set.contains(s.charAt(index))){
+            //字符不重复，则index右移
+            while (index < s.length() && !set.contains(s.charAt(index))) {
                 set.add(s.charAt(index));
                 index++;
             }
-            result = Math.max(result, index - i +1);
+            result = Math.max(result, index - i);
         }
         return result;
     }
+
     //88. 合并两个有序数组
     //给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
     //初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
@@ -331,14 +337,14 @@ public class NacosPassword {
         int p2 = n - 1;
         int tail = m + n - 1;
         int cur;
-        while(p1 >= 0 || p2 >= 0){
-            if(p1 == -1){
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
                 cur = nums2[p2--];
-            }else if(p2 == -1){
+            } else if (p2 == -1) {
                 cur = nums1[p1--];
-            }else if(nums1[p1] > nums2[p2]){
+            } else if (nums1[p1] > nums2[p2]) {
                 cur = nums1[p1--];
-            }else {
+            } else {
                 cur = nums2[p2--];
             }
             nums1[tail--] = cur;
