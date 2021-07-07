e8d0878ec7efa66d8be6f6b7a502974abde3825c
hui <fwu@today36524.com>
2021-07-07 16:42:18 +0800

    add 合并两个有序数组 (简单 逆序双指针)

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -288,13 +288,52 @@ public class NacosPassword {
             this.next = next;
         }
     }
+
+    //88. 合并两个有序数组
+    //给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+    //初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int index1 = 0;
+//        int index2 = 0;
+//        while (index1 < m && index2 < n) {
+//            if (nums1[index1] <= nums2[index2]) {
+//                index1++;
+//            } else {
+//                int temp = nums1[index1];
+//                nums1[index1] = nums2[index2];
+//                nums2[index2] = temp;
+//                index1++;
+//                Arrays.sort(nums2);
+//            }
+//        }
+//        while (index1 < (m + n) && index2 < n) {
+//            nums1[index1++] = nums2[index2++];
+//        }
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while(p1 >= 0 || p2 >= 0){
+            if(p1 == -1){
+                cur = nums2[p2--];
+            }else if(p2 == -1){
+                cur = nums1[p1--];
+            }else if(nums1[p1] > nums2[p2]){
+                cur = nums1[p1--];
+            }else {
+                cur = nums2[p2--];
+            }
+            nums1[tail--] = cur;
+        }
+    }
+
     //1. 两数之和
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int temp = target - nums[i];
-            if(hashMap.containsKey(temp)){
+            if (hashMap.containsKey(temp)) {
                 result[0] = hashMap.get(temp);
                 result[1] = i;
                 return result;
@@ -303,6 +342,7 @@ public class NacosPassword {
         }
         return null;
     }
+
     //1711. 大餐计数
     //大餐 是指 恰好包含两道不同餐品 的一餐，其美味程度之和等于 2 的幂。
     //你可以搭配 任意 两道餐品做一顿大餐。
@@ -328,7 +368,8 @@ public class NacosPassword {
         }
         return pairs;
     }
-//645. 错误的集合
+
+    //645. 错误的集合
 //集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
 //    给定一个数组 nums 代表了集合 S 发生错误后的结果。
 //    请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
