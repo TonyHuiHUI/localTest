90a1e9bd4735f262d8aa84e5af81ef32a212b3e2
hui <fwu@today36524.com>
2021-07-08 15:35:42 +0800

    add 买卖股票的最佳时机 （简单 dp）

--- src/main/java/NacosPassword.java
+++ src/main/java/NacosPassword.java
@@ -293,6 +293,21 @@ public class NacosPassword {
         }
     }
 
+    //121. 买卖股票的最佳时机
+    //给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+//你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+//返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int max = 0;//最大利润
+        int min = prices[0];//遍历到的最小价格
+        for (int i = 1; i < n; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
+        }
+        return max;
+    }
+
     //350. 两个数组的交集 II
     //给定两个数组，编写一个函数来计算它们的交集
     public static int[] intersect(int[] nums1, int[] nums2) {
