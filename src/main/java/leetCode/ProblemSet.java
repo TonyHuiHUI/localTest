101d28fed2be37eaad9f97acd09255f6b646db10
hui <wufeizz@qq.com>
2021-07-11 17:03:26 +0800

    add 有效的数独（中等 数组模拟）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -7,7 +7,36 @@ public class ProblemSet {
         int[] a = {3,0,6,1,5};
         System.out.println(hIndex(a));
     }
-
+    //36. 有效的数独
+    //请你判断一个 9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+    //数字 1-9 在每一行只能出现一次。
+    //数字 1-9 在每一列只能出现一次。
+    //数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+    //数独部分空格内已填入了数字，空白格用 '.' 表示。
+    //注意：
+    //一个有效的数独（部分已被填充）不一定是可解的。
+    //只需要根据以上规则，验证已经填入的数字是否有效即可
+    public static boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+        for (int i = 0; i < 9;i++){
+            for (int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int num = board[i][j] - '1';
+                    int boxIndex = i / 3 * 3 + j / 3;
+                    if(row[i][num] || col[j][num] || box[boxIndex][num]){
+                        return false;
+                    }else {
+                        row[i][num] = true;
+                        col[j][num] = true;
+                        box[boxIndex][num] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
     //274. H 指数
     //给定一位研究者论文被引用次数的数组（被引用次数是非负整数）。编写一个方法，计算出研究者的 h 指数。
     //h 指数的定义：h 代表“高引用次数”（high citations），一名科研人员的 h 指数是指他（她）的 （N 篇论文中）总共有 h 篇论文分别被引用了至少 h 次。且其余的 N - h 篇论文每篇被引用次数 不超过 h 次。
