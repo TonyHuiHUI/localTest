ab57b131d0197bd80796c8148c26f28890919aa6
hui <fwu@today36524.com>
2021-07-12 14:45:50 +0800

    fix 合并有序链表 (简单 递归)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -53,20 +53,33 @@ public class ProblemSet {
     //    21. 合并两个有序链表
     //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        ListNode cur = head;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
+        //迭代
+//        ListNode head = new ListNode();
+//        ListNode cur = head;
+//        while (l1 != null && l2 != null) {
+//            if (l1.val <= l2.val) {
+//                cur.next = l1;
+//                l1 = l1.next;
+//            } else {
+//                cur.next = l2;
+//                l2 = l2.next;
+//            }
+//            cur = cur.next;
+//        }
+//        cur.next = l1 == null ? l2 : l1;
+//        return head.next;
+        //递归
+        if(l1 == null){
+            return l1;
+        }else if(l2 == null){
+            return l2;
+        }else if(l1.val <= l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        cur.next = l1 == null ? l2 : l1;
-        return head.next;
     }
 
     //141. 环形链表
