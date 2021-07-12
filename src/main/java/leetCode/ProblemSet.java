4fe692c800ed31736405b5eaa8785e0d04f96a4f
hui <fwu@today36524.com>
2021-07-12 14:27:11 +0800

    fix 合并有序链表 (简单 迭代)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -5,28 +5,57 @@ import java.util.HashMap;
 
 public class ProblemSet {
     public static void main(String[] args) {
-        int[] a = {3,0,6,1,5};
+        int[] a = {3, 0, 6, 1, 5};
         System.out.println(hIndex(a));
     }
-    public class ListNode {
+
+    public static class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
+
+        ListNode() {
         }
 
+        ListNode(int x) {
+            this.val = x;
+            this.next = null;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
+    //    21. 合并两个有序链表
+    //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return head.next;
+    }
+
     //141. 环形链表
     //给定一个链表，判断链表中是否有环。
     public static boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return false;
         }
         ListNode slow = head;
         ListNode fast = head.next;
-        while (fast != slow ){
-            if(fast == null || fast.next == null){
+        while (fast != slow) {
+            if (fast == null || fast.next == null) {
                 return false;
             }
             slow = slow.next;
@@ -34,6 +63,7 @@ public class ProblemSet {
         }
         return true;
     }
+
     //242. 有效的字母异位词
     //给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
     public boolean isAnagram(String s, String t) {
@@ -52,34 +82,37 @@ public class ProblemSet {
         }
         return true;
     }
+
     //383. 赎金信
     //给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。
     public static boolean canConstruct(String ransomNote, String magazine) {
         int[] hash = new int[26];
-        for (int i = 0; i < magazine.length(); i++){
+        for (int i = 0; i < magazine.length(); i++) {
             hash[magazine.charAt(i) - 'a']++;
         }
-        for(int i = 0; i < ransomNote.length(); i++){
-            if(--hash[ransomNote.charAt(i) - 'a'] < 0){
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--hash[ransomNote.charAt(i) - 'a'] < 0) {
                 return false;
             }
         }
         return true;
     }
+
     //387. 字符串中的第一个唯一字符
     //给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
     public static int firstUniqChar(String s) {
-        HashMap<Character,Integer> hashMap = new HashMap<>();
-        for (int i = 0 ;i < s.length(); i++){
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
             hashMap.put(s.charAt(i), hashMap.getOrDefault(s.charAt(i), 0) + 1);
         }
-        for(int i = 0; i < s.length(); i++){
-            if(hashMap.get(s.charAt(i)) == 1){
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
         return -1;
     }
+
     //275. H 指数 II
     //给定一位研究者论文被引用次数的数组（被引用次数是非负整数），数组已经按照 升序排列 。编写一个方法，计算出研究者的 h 指数。
     public static int hIndex2(int[] citations) {
@@ -103,6 +136,7 @@ public class ProblemSet {
         }
         return n - left;
     }
+
     //73. 矩阵置零
 //    给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
     public static void setZeroes(int[][] matrix) {
@@ -110,46 +144,47 @@ public class ProblemSet {
         int m = matrix[0].length;
         boolean row0 = false;//第一行是否有0
         boolean col0 = false;//第一列是否有0
-        for(int i = 0; i < n; i++){
-            if(matrix[i][0] == 0){
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
                 col0 = true;
             }
         }
-        for(int i = 0; i < m; i++){
-            if(matrix[0][i] == 0){
+        for (int i = 0; i < m; i++) {
+            if (matrix[0][i] == 0) {
                 row0 = true;
             }
         }
         //利用第一行和第一列记录矩阵是否有0
-        for(int i = 1; i < n; i++){
-            for (int j = 1; j < m; j++){
-                if(matrix[i][j] == 0){
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
         //覆盖原矩阵
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
         //还原原第一行
-        if(row0){
-            for(int i = 0; i < m; i++){
+        if (row0) {
+            for (int i = 0; i < m; i++) {
                 matrix[0][i] = 0;
             }
         }
         //还原原第一列
-        if(col0){
-            for(int i = 0; i < n; i++){
+        if (col0) {
+            for (int i = 0; i < n; i++) {
                 matrix[i][0] = 0;
             }
         }
     }
+
     //36. 有效的数独
     //请你判断一个 9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
     //数字 1-9 在每一行只能出现一次。
@@ -163,14 +198,14 @@ public class ProblemSet {
         boolean[][] row = new boolean[9][9];
         boolean[][] col = new boolean[9][9];
         boolean[][] box = new boolean[9][9];
-        for (int i = 0; i < 9;i++){
-            for (int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
                     int num = board[i][j] - '1';
                     int boxIndex = i / 3 * 3 + j / 3;
-                    if(row[i][num] || col[j][num] || box[boxIndex][num]){
+                    if (row[i][num] || col[j][num] || box[boxIndex][num]) {
                         return false;
-                    }else {
+                    } else {
                         row[i][num] = true;
                         col[j][num] = true;
                         box[boxIndex][num] = true;
@@ -180,6 +215,7 @@ public class ProblemSet {
         }
         return true;
     }
+
     //274. H 指数
     //给定一位研究者论文被引用次数的数组（被引用次数是非负整数）。编写一个方法，计算出研究者的 h 指数。
     //h 指数的定义：h 代表“高引用次数”（high citations），一名科研人员的 h 指数是指他（她）的 （N 篇论文中）总共有 h 篇论文分别被引用了至少 h 次。且其余的 N - h 篇论文每篇被引用次数 不超过 h 次。
@@ -187,9 +223,9 @@ public class ProblemSet {
     public static int hIndex(int[] citations) {
         Arrays.sort(citations);
         int len = citations.length;
-        for(int i = 0 ; i < len; i++){
+        for (int i = 0; i < len; i++) {
             int h = len - i;
-            if(citations[i] >= h){
+            if (citations[i] >= h) {
                 return h;
             }
         }
