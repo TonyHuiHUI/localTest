5a025f429cd3de499a3f1f416ac3ce425ff24f76
hui <fwu@today36524.com>
2021-07-12 11:45:50 +0800

    add 赎金信 (简单 hash)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -8,6 +8,20 @@ public class ProblemSet {
         int[] a = {3,0,6,1,5};
         System.out.println(hIndex(a));
     }
+    //383. 赎金信
+    //给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] hash = new int[26];
+        for (int i = 0; i < magazine.length(); i++){
+            hash[magazine.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < ransomNote.length(); i++){
+            if(--hash[ransomNote.charAt(i) - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
     //387. 字符串中的第一个唯一字符
     //给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
     public static int firstUniqChar(String s) {
