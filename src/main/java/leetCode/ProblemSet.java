a80cfc96da317437691999b8c4fc062ab1abaf60
hui <fwu@today36524.com>
2021-07-12 14:39:14 +0800

    fix 移除链表元素 (简单 迭代)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -27,6 +27,22 @@ public class ProblemSet {
         }
     }
 
+    //203. 移除链表元素
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        while (cur.next != null) {
+            if(cur.next.val == val){
+                cur.next = cur.next.next;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
     //    21. 合并两个有序链表
     //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -36,7 +52,7 @@ public class ProblemSet {
             if (l1.val <= l2.val) {
                 cur.next = l1;
                 l1 = l1.next;
-            }else {
+            } else {
                 cur.next = l2;
                 l2 = l2.next;
             }
