a08151094a20f90824df4ab881289fed6a3a7591
hui <fwu@today36524.com>
2021-07-13 14:39:27 +0800

    add  天际线问题 (困难 扫描线+优先队列)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -1,12 +1,13 @@
 package leetCode;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class ProblemSet {
     public static void main(String[] args) {
-        int[] a = {3, 0, 6, 1, 5};
-        System.out.println(hIndex(a));
+//        int[] a = {3, 0, 6, 1, 5};
+//        System.out.println(hIndex(a));
+        int[][] a = {{2,9,10},{3,7,15},{5,12,12},{15,20,10},{19,24,8}};
+        System.out.println(getSkyline(a));
     }
 
     public static class ListNode {
@@ -26,7 +27,38 @@ public class ProblemSet {
             this.next = next;
         }
     }
-
+    //218. 天际线问题
+    //城市的天际线是从远处观看该城市中所有建筑物形成的轮廓的外部轮廓。给你所有建筑物的位置和高度，请返回由这些建筑物形成的 天际线 。
+    //每个建筑物的几何信息由数组 buildings 表示，其中三元组 buildings[i] = [lefti, righti, heighti] 表示：
+    //    lefti 是第 i 座建筑物左边缘的 x 坐标。
+    //    righti 是第 i 座建筑物右边缘的 x 坐标。
+    //    heighti 是第 i 座建筑物的高度。
+    //天际线 应该表示为由 “关键点” 组成的列表，格式 [[x1,y1],[x2,y2],...] ，并按 x 坐标 进行 排序 。关键点是水平线段的左端点。列表中最后一个点是最右侧建筑物的终点，y 坐标始终为 0 ，仅用于标记天际线的终点。此外，任何两个相邻建筑物之间的地面都应被视为天际线轮廓的一部分。
+    public static List<List<Integer>> getSkyline(int[][] buildings) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> b[1] - a[1]);
+        List<Integer> boundaries = new ArrayList<Integer>();
+        for (int[] building : buildings) {
+            boundaries.add(building[0]);
+            boundaries.add(building[1]);
+        }
+        Collections.sort(boundaries);
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        int n = buildings.length, idx = 0;
+        for (int boundary : boundaries) {
+            while (idx < n && buildings[idx][0] <= boundary) {
+                pq.offer(new int[]{buildings[idx][1], buildings[idx][2]});
+                idx++;
+            }
+            while (!pq.isEmpty() && pq.peek()[0] <= boundary) {
+                pq.poll();
+            }
+            int maxn = pq.isEmpty() ? 0 : pq.peek()[1];
+            if (ret.size() == 0 || maxn != ret.get(ret.size() - 1).get(1)) {
+                ret.add(Arrays.asList(boundary, maxn));
+            }
+        }
+        return ret;
+    }
     //203. 移除链表元素
 //给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
     public static ListNode removeElements(ListNode head, int val) {
