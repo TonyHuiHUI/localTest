1d0befcc00c7ea01e03f060435131e1dd45e7601
hui <fwu@today36524.com>
2021-07-13 15:38:41 +0800

    add  删除排序链表中的重复元素（简单 迭代)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -27,6 +27,23 @@ public class ProblemSet {
             this.next = next;
         }
     }
+    //83. 删除排序链表中的重复元素
+    //存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+    //返回同样按升序排列的结果链表。
+    public static  ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode cur = head;
+        while (cur.next != null){
+            if(cur.val == cur.next.val){
+                cur.next = cur.next.next;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
     //206. 反转链表
     //给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
     public static ListNode reverseList(ListNode head) {
