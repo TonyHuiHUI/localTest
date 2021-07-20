f476222a8158a267f3c0076b11bad54960a2b05b
hui <fwu@today36524.com>
2021-07-20 10:52:14 +0800

    数组中最大数对和的最小值（中等 排序）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -39,6 +39,22 @@ public class ProblemSet {
             this.right = right;
         }
     }
+    //1877. 数组中最大数对和的最小值
+//    一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+//    比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+//    给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+//    nums 中每个元素 恰好 在 一个 数对中，且
+//    最大数对和 的值 最小 。
+//    请你在最优数对划分的方案下，返回最小的 最大数对和 。
+    public static int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        int n = nums.length;
+        for (int i = 0; i < n/2; i++){
+            max = Math.max(max, nums[i] + nums[n - i - 1]);
+        }
+       return max;
+    }
     //98. 验证二叉搜索树
     public static boolean isValidBST(TreeNode root) {
         //递归
