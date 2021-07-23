d3f0f468cc072a2d962e910dc46a591a9e7b6053
hui <fwu@today36524.com>
2021-07-23 15:43:54 +0800

    检查是否区域内所有整数都被覆盖（简单 排序|查分数组）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -17,8 +17,10 @@ public class ProblemSet {
 //        postorderTraversal(root);
 //        int[] a = {5,7,7,8,8,10};
 //        System.out.println(search(a,8));
-        int[] a = {1, 4, 8, 13};
-        System.out.println(maxFrequency(a, 5));
+//        int[] a = {1, 4, 8, 13};
+//        System.out.println(maxFrequency(a, 5));
+        int[][] a = {{1,10},{10,20}};
+        System.out.println(isCovered(a,21, 21));
     }
 
     public static class TreeNode {
@@ -39,16 +41,46 @@ public class ProblemSet {
             this.right = right;
         }
     }
-//    138. 复制带随机指针的链表
+    //1893. 检查是否区域内所有整数都被覆盖
+    //给你一个二维整数数组 ranges 和两个整数 left 和 right 。每个 ranges[i] = [starti, endi] 表示一个从 starti 到 endi 的 闭区间 。
+    //如果闭区间 [left, right] 内每个整数都被 ranges 中 至少一个 区间覆盖，那么请你返回 true ，否则返回 false 。
+    //已知区间 ranges[i] = [starti, endi] ，如果整数 x 满足 starti <= x <= endi ，那么我们称整数x 被覆盖了。
+    public static boolean isCovered(int[][] ranges, int left, int right) {
+        //排序
+//        Arrays.sort(ranges, (a1, a2) -> a1[0] - a2[0]);
+//        for(int[] range : ranges){
+//            if(range[0] <= left && left <= range[1]){
+//                left = range[1] + 1;
+//            }
+//        }
+//        return left > right;
+        int[] diff = new int[52];   // 差分数组
+        for (int[] range : ranges) {
+            ++diff[range[0]];
+            --diff[range[1] + 1];
+        }
+        // 前缀和
+        int curr = 0;
+        for (int i = 1; i <= 50; ++i) {
+            curr += diff[i];
+            if (i >= left && i <= right && curr <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //    138. 复制带随机指针的链表
     public ListNode copyRandomList(ListNode head) {
         HashMap<ListNode, ListNode> hashMap = new HashMap<>();
         return helper(head, hashMap);
     }
-    public ListNode helper(ListNode head, HashMap<ListNode, ListNode> hashMap){
-        if(head == null){
+
+    public ListNode helper(ListNode head, HashMap<ListNode, ListNode> hashMap) {
+        if (head == null) {
             return null;
         }
-        if(!hashMap.containsKey(head)){
+        if (!hashMap.containsKey(head)) {
             ListNode node = new ListNode(head.val);
             hashMap.put(head, node);
             node.next = helper(head.next, hashMap);
@@ -56,6 +88,7 @@ public class ProblemSet {
         }
         return hashMap.get(head);
     }
+
     //    剑指 Offer 52. 两个链表的第一个公共节点
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode pa = headA;
