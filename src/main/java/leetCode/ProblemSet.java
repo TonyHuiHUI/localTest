acad46fd4c6278cf6c8d57526c485d68a9bfa7dd
hui <fwu@today36524.com>
2021-08-24 11:24:26 +0800

    add K站中转内最便宜的航班（mid dp)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -73,7 +73,29 @@ public class ProblemSet {
             this.right = right;
         }
     }
-
+    //787. K 站中转内最便宜的航班
+    //有 n 个城市通过一些航班连接。给你一个数组 flights ，其中 flights[i] = [fromi, toi, pricei] ，表示该航班都从城市 fromi 开始，以价格 toi 抵达 pricei。
+    //现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到出一条最多经过 k 站中转的路线，使得从 src 到 dst 的 价格最便宜 ，并返回该价格。 如果不存在这样的路线，则输出 -1
+    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        //dp
+        final int INF = 10000 * 101 + 1;
+        int[][] f = new int[k + 2][n];// f[t][i]表示通过恰好 t 次航班，从出发城市 src 到达城市 i需要的最小花费
+        for (int i = 0; i < k + 2; ++i) {
+            Arrays.fill(f[i], INF);
+        }
+        f[0][src] = 0;
+        for (int t = 1; t <= k + 1; ++t) {
+            for (int[] flight : flights) {
+                int j = flight[0], i = flight[1], cost = flight[2];
+                f[t][i] = Math.min(f[t][i], f[t - 1][j] + cost);
+            }
+        }
+        int ans = INF;
+        for (int t = 1; t <= k + 1; ++t) {
+            ans = Math.min(ans, f[t][dst]);
+        }
+        return ans == INF ? -1 : ans;
+    }
     //1646. 获取生成数组中的最大值
     //给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
 //    nums[0] = 0
