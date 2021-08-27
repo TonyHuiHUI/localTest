8321b8548697668d8ad7b622e94677b11e1ce698
hui <fwu@today36524.com>
2021-08-27 13:22:51 +0800

    add 数据流中的中位数（hard 双优先队列）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -55,7 +55,7 @@ public class ProblemSet {
 //        System.out.println(countArrangement(2));
 //        int[][] a = {{1,2},{3},{3},{}};
 //        System.out.println(allPathsSourceTarget(a));
-        int[] a = {5,1,4,2};
+        int[] a = {5, 1, 4, 2};
         System.out.println(numRescueBoats(a, 6));
     }
 
@@ -75,6 +75,48 @@ public class ProblemSet {
             this.val = val;
             this.left = left;
             this.right = right;
+        }
+    }
+
+    //295. 数据流的中位数
+    //中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。
+    //例如，
+    //[2,3,4] 的中位数是 3
+    //[2,3] 的中位数是 (2 + 3) / 2 = 2.5
+    //设计一个支持以下两种操作的数据结构：
+    //    void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+    //    double findMedian() - 返回目前所有元素的中位数。
+    class MedianFinder {
+        PriorityQueue<Integer> queueMin;
+        PriorityQueue<Integer> queueMax;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MedianFinder() {
+            queueMin = new PriorityQueue<Integer>((a, b) -> (b - a));
+            queueMax = new PriorityQueue<Integer>((a, b) -> (a - b));
+        }
+
+        public void addNum(int num) {
+            if (queueMin.isEmpty() || num <= queueMin.peek()) {
+                queueMin.offer(num);
+                if (queueMin.size() > queueMax.size() + 1) {
+                    queueMax.offer(queueMin.poll());
+                }
+            } else {
+                queueMax.offer(num);
+                if (queueMax.size() > queueMin.size()) {
+                    queueMin.offer(queueMax.poll());
+                }
+            }
+        }
+
+        public double findMedian() {
+            if (queueMin.size() > queueMax.size()) {
+                return queueMin.peek();
+            }
+            return (queueMin.peek() + queueMax.peek()) / 2.0;
         }
     }
 
