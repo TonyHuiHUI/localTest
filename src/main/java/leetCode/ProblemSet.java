8fcfa43ce1b067306fcd6bca6628536e56f71dad
hui <fwu@today36524.com>
2021-09-01 10:29:39 +0800

    add 比较版本号（mid 字符串分割）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -87,6 +87,34 @@ public class ProblemSet {
         }
     }
 
+    //165. 比较版本号
+    //给你两个版本号 version1 和 version2 ，请你比较它们。
+    //版本号由一个或多个修订号组成，各修订号由一个 '.' 连接。每个修订号由 多位数字 组成，可能包含 前导零 。每个版本号至少包含一个字符。修订号从左到右编号，下标从 0 开始，最左边的修订号下标为 0 ，下一个修订号下标为 1 ，以此类推。例如，2.5.33 和 0.1 都是有效的版本号。
+    //比较版本号时，请按从左到右的顺序依次比较它们的修订号。比较修订号时，只需比较 忽略任何前导零后的整数值 。也就是说，修订号 1 和修订号 001 相等 。如果版本号没有指定某个下标处的修订号，则该修订号视为 0 。例如，版本 1.0 小于版本 1.1 ，因为它们下标为 0 的修订号相同，而下标为 1 的修订号分别为 0 和 1 ，0 < 1 。
+    //返回规则如下：
+    //    如果 version1 > version2 返回 1，
+    //    如果 version1 < version2 返回 -1，
+    //    除此之外返回 0。
+    public static int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for(int i = 0; i < v1.length || i < v2.length; i++){
+            int x1 = 0;
+            int x2 = 0;
+            if(i < v1.length){
+                x1 = Integer.valueOf(v1[i]);
+            }
+            if(i < v2.length){
+                x2 = Integer.valueOf(v2[i]);
+            }
+            if(x1 < x2){
+                return -1;
+            }else if(x1 > x2){
+                return 1;
+            }
+        }
+        return 0;
+    }
     //1109. 航班预订统计
     //这里有 n 个航班，它们分别从 1 到 n 进行编号。
     //有一份航班预订表 bookings ，表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi] 意味着在从 firsti 到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。
