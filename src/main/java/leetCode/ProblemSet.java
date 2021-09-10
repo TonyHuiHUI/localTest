b096befe3972369278768dbafbc20ffe8f87e8db
hui <fwu@today36524.com>
2021-09-10 10:19:11 +0800

    add 找到需要补充粉笔的学生编号 (mid 模拟)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -1681,7 +1681,27 @@ public class ProblemSet {
             this.next = next;
         }
     }
-
+    //1894. 找到需要补充粉笔的学生编号
+    //一个班级里有 n 个学生，编号为 0 到 n - 1 。每个学生会依次回答问题，编号为 0 的学生先回答，然后是编号为 1 的学生，以此类推，直到编号为 n - 1 的学生，然后老师会重复这个过程，重新从编号为 0 的学生开始回答问题。
+    //给你一个长度为 n 且下标从 0 开始的整数数组 chalk 和一个整数 k 。一开始粉笔盒里总共有 k 支粉笔。当编号为 i 的学生回答问题时，他会消耗 chalk[i] 支粉笔。如果剩余粉笔数量 严格小于 chalk[i] ，那么学生 i 需要 补充 粉笔。
+    //请你返回需要 补充 粉笔的学生 编号 。
+    public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        long total = 0;
+        for (int i: chalk) {
+            total += i;
+        }
+        k %= total;
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            if(chalk[i] > k){
+                result = i;
+                break;
+            }
+            k -= chalk[i];
+        }
+        return result;
+    }
 //    68. 文本左右对齐
 //    给定一个单词数组和一个长度 maxWidth，重新排版单词，使其成为每行恰好有 maxWidth 个字符，且左右两端对齐的文本。
 //    你应该使用“贪心算法”来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth 个字符。
