ac6ea6ff373f06435bef96fa578baec297166065
hui <fwu@today36524.com>
2021-09-13 10:45:53 +0800

    add 回旋镖的数量 (mid 枚举+哈希)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -1681,6 +1681,24 @@ public class ProblemSet {
             this.next = next;
         }
     }
+    //447. 回旋镖的数量
+    //给定平面上 n 对 互不相同 的点 points ，其中 points[i] = [xi, yi] 。回旋镖 是由点 (i, j, k) 表示的元组 ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+    //返回平面上所有回旋镖的数量。
+    public int numberOfBoomerangs(int[][] points) {
+        int result = 0;
+        for(int[] x: points){
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int[] y : points){
+                int dis = (x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1]);
+                map.put(dis, map.getOrDefault(dis,0) + 1);
+            }
+            for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+                int n = entry.getValue();
+                result += n * (n - 1);
+            }
+        }
+        return result;
+    }
     //1894. 找到需要补充粉笔的学生编号
     //一个班级里有 n 个学生，编号为 0 到 n - 1 。每个学生会依次回答问题，编号为 0 的学生先回答，然后是编号为 1 的学生，以此类推，直到编号为 n - 1 的学生，然后老师会重复这个过程，重新从编号为 0 的学生开始回答问题。
     //给你一个长度为 n 且下标从 0 开始的整数数组 chalk 和一个整数 k 。一开始粉笔盒里总共有 k 支粉笔。当编号为 i 的学生回答问题时，他会消耗 chalk[i] 支粉笔。如果剩余粉笔数量 严格小于 chalk[i] ，那么学生 i 需要 补充 粉笔。
