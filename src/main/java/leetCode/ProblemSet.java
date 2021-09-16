e0b656951c2388e503f906c5b07ea9a445ae8dec
hui <fwu@today36524.com>
2021-09-16 14:22:13 +0800

    add 单词搜索II(hard 字典树+回溯)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -1,6 +1,5 @@
 package leetCode;
 
-import scala.io.BytePickle;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
@@ -63,9 +62,11 @@ public class ProblemSet {
 //        System.out.println(solution.pickIndex());
 //        System.out.println(solution.pickIndex());
 //        System.out.println(solution.pickIndex());
-        int[][] a = {{1, 2, 10}, {2, 3, 20}, {2, 5, 25}};
-        System.out.println(corpFlightBookings(a, 5));
-
+//        int[][] a = {{1, 2, 10}, {2, 3, 20}, {2, 5, 25}};
+//        System.out.println(corpFlightBookings(a, 5));
+        char[][] a = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
+        String[] b = {"oath","pea","eat","rain"};
+        System.out.println(findWords(a, b));
     }
 
     public static class TreeNode {
@@ -85,6 +86,64 @@ public class ProblemSet {
             this.left = left;
             this.right = right;
         }
+    }
+    //212. 单词搜索 II
+    //给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words，找出所有同时在二维网格和字典中出现的单词。
+    //单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+    static class Trie {//字典树
+        String word;
+        Map<Character,Trie> child;
+        public Trie(){
+            this.word = "";
+            this.child = new HashMap<>();
+        }
+        public void insert(String word){
+            int n = word.length();
+            Trie cur = this;
+            for(int i = 0; i < n; i++){
+                char c = word.charAt(i);
+                if(!cur.child.containsKey(c)){
+                    cur.child.put(c,new Trie());
+                }
+                cur = cur.child.get(c);
+            }
+            cur.word = word;
+        }
+    }
+    static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public static List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+        Set<String> ans = new HashSet<>();
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[0].length; ++j) {
+                dfs(board, trie, i, j, ans);
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+
+    public static void dfs(char[][] board, Trie now, int i1, int j1, Set<String> ans) {
+        if (!now.child.containsKey(board[i1][j1])) {
+            return;
+        }
+        char ch = board[i1][j1];
+        now = now.child.get(ch);
+        if (!"".equals(now.word)) {
+            ans.add(now.word);
+        }
+
+        board[i1][j1] = '#';
+        for (int[] dir : dirs) {
+            int i2 = i1 + dir[0], j2 = j1 + dir[1];
+            if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length) {
+                dfs(board, now, i2, j2, ans);
+            }
+        }
+        board[i1][j1] = ch;
     }
 
     //剑指 Offer 22. 链表中倒数第k个节点
