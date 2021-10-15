bfb82d5c03cc6daea6d808f9a6208a52744ac956
hui <fwu@today36524.com>
2021-10-15 10:30:33 +0800

    add 外观数列（mid 遍历|递归）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -99,6 +99,61 @@ public class ProblemSet {
         public Node child;
     }
 
+    //38. 外观数列
+    //给定一个正整数 n ，输出外观数列的第 n 项。
+    //「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+    //你可以将其视作是由递归公式定义的数字字符串序列：
+    //    countAndSay(1) = "1"
+    //    countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+    //前五项如下：
+    //1.     1
+    //2.     11
+    //3.     21
+    //4.     1211
+    //5.     111221
+    //第一项是数字 1
+    //描述前一项，这个数是 1 即 “ 一 个 1 ”，记作 "11"
+    //描述前一项，这个数是 11 即 “ 二 个 1 ” ，记作 "21"
+    //描述前一项，这个数是 21 即 “ 一 个 2 + 一 个 1 ” ，记作 "1211"
+    //描述前一项，这个数是 1211 即 “ 一 个 1 + 一 个 2 + 二 个 1 ” ，记作 "111221"
+    //要 描述 一个数字字符串，首先要将字符串分割为 最小 数量的组，每个组都由连续的最多 相同字符 组成。然后对于每个组，先描述字符的数量，然后描述字符，形成一个描述组。要将描述转换为数字字符串，先将每组中的字符数量用数字替换，再将所有描述组连接起来。
+    public String countAndSay(int n) {
+        //递归
+//        if (n == 1) {
+//            return "1";
+//        }
+//        String pre = countAndSay(n - 1);
+//        StringBuilder sb = new StringBuilder();
+//        int length = pre.length();
+//        int pos = 0, start = 0;
+//        while (pos < length) {
+//            while (pos < length && pre.charAt(pos) == pre.charAt(start)) {
+//                pos++;
+//            }
+//            sb.append(Integer.toString(pos - start)).append(pre.charAt(start));
+//            start = pos;
+//        }
+//        return sb.toString();
+
+        //遍历
+        String str = "1";
+        for (int i = 2; i <= n; ++i) {
+            StringBuilder sb = new StringBuilder();
+            int start = 0;
+            int pos = 0;
+
+            while (pos < str.length()) {
+                while (pos < str.length() && str.charAt(pos) == str.charAt(start)) {
+                    pos++;
+                }
+                sb.append(Integer.toString(pos - start)).append(str.charAt(start));
+                start = pos;
+            }
+            str = sb.toString();
+        }
+        return str;
+    }
+
     //剑指 Offer II 069. 山峰数组的顶部
     //符合下列属性的数组 arr 称为 山峰数组（山脉数组） ：
     //    arr.length >= 3
@@ -121,12 +176,12 @@ public class ProblemSet {
         int n = arr.length;
         int result = -1;
         int left = 1, right = n - 2;
-        while (left <= right){
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if(arr[mid] > arr[mid + 1]){
+            if (arr[mid] > arr[mid + 1]) {
                 result = mid;
                 right = mid - 1;
-            }else {
+            } else {
                 left = mid + 1;
             }
         }
