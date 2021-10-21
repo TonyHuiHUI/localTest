797163ab1cd75311e593f248e596aaeb972bf732
hui <fwu@today36524.com>
2021-10-21 10:56:31 +0800

    add 加一（easy 数学）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -124,7 +124,22 @@ public class ProblemSet {
             curPos.isEnd = true;
         }
     }
-
+    //66. 加一
+    //给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+    //最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+    //你可以假设除了整数 0 之外，这个整数不会以零开头。
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >=0; i--){
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if(digits[i] != 0){
+                return digits;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
 //    453. 最小操作次数使数组元素相等
 //给你一个长度为 n 的整数数组，每次操作将会使 n - 1 个元素增加 1 。返回让数组所有元素相等的最小操作次数。
         public int minMoves(int[] nums) {
