b8f93a282122bbf0aabbea5e75f995c2a82f0334
hui <fwu@today36524.com>
2021-11-11 10:59:38 +0800

    add K个逆序对数组（hard dp+前缀和）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -128,7 +128,24 @@ public class ProblemSet {
             curPos.isEnd = true;
         }
     }
-
+    //629. K个逆序对数组
+//    给出两个整数 n 和 k，找出所有包含从 1 到 n 的数字，且恰好拥有 k 个逆序对的不同的数组的个数。
+    //逆序对的定义如下：对于数组的第i个和第 j个元素，如果满i < j且 a[i] > a[j]，则其为一个逆序对；否则不是。
+    //由于答案可能很大，只需要返回 答案 mod 109 + 7 的值。
+    public int kInversePairs(int n, int k) {
+        int mod = (int)1e9+7;
+        int[][] f = new int[n + 1][k + 1];
+        int[][] sum = new int[n + 1][k + 1];
+        f[1][0] = 1;
+        Arrays.fill(sum[1], 1);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                f[i][j] = j < i ? sum[i - 1][j] : (sum[i - 1][j] - sum[i - 1][j - (i - 1) - 1] + mod) % mod;
+                sum[i][j] = j == 0 ? f[i][j] : (sum[i][j - 1] + f[i][j]) % mod;
+            }
+        }
+        return f[n][k];
+    }
     //495. 提莫攻击
 //    给你一个 非递减 的整数数组 timeSeries ，其中 timeSeries[i] 表示提莫在 timeSeries[i] 秒时对艾希发起攻击，以及一个表示中毒持续时间的整数 duration 。
 //    返回艾希处于中毒状态的 总 秒数。
