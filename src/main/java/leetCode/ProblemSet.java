d77cfd4402d078bada4dfe544f539f9deb8552f1
hui <fwu@today36524.com>
2021-11-22 13:18:10 +0800

    add 大乱数组（mid 洗牌算法）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -108,6 +108,39 @@ public class ProblemSet {
         public Node child;
     }
 
+    //384. 打乱数组
+    //给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。
+    //实现 Solution class:
+    //    Solution(int[] nums) 使用整数数组 nums 初始化对象
+    //    int[] reset() 重设数组到它的初始状态并返回
+    //    int[] shuffle() 返回数组随机打乱后的结果
+    class Solution1 {
+        int[] nums;
+        int[] original;
+
+        public Solution1(int[] nums) {
+            this.nums = nums;
+            this.original = new int[nums.length];
+            System.arraycopy(nums, 0, original, 0, nums.length);
+        }
+
+        public int[] reset() {
+            System.arraycopy(original, 0, nums, 0, nums.length);
+            return nums;
+        }
+
+        public int[] shuffle() {
+            Random random = new Random();
+            for (int i = 0; i < nums.length; i++) {
+                int j = i + random.nextInt(nums.length - i);
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+            return nums;
+        }
+    }
+
     //字典树
     class TrieD {
         TrieD[] next;
