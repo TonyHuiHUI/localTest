5b145ffc6b8363900b506c5e25d9d0fdf7cf6512
hui <fwu@today36524.com>
2021-12-02 10:08:42 +0800

    add 相对名次（easy 排序）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -109,6 +109,34 @@ public class ProblemSet {
         public Node child;
     }
 
+    //506. 相对名次
+    //给你一个长度为 n 的整数数组 score ，其中 score[i] 是第 i 位运动员在比赛中的得分。所有得分都 互不相同 。
+    //运动员将根据得分 决定名次 ，其中名次第 1 的运动员得分最高，名次第 2 的运动员得分第 2 高，依此类推。运动员的名次决定了他们的获奖情况：
+    //    名次第 1 的运动员获金牌 "Gold Medal" 。
+    //    名次第 2 的运动员获银牌 "Silver Medal" 。
+    //    名次第 3 的运动员获铜牌 "Bronze Medal" 。
+    //    从名次第 4 到第 n 的运动员，只能获得他们的名次编号（即，名次第 x 的运动员获得编号 "x"）。
+    //使用长度为 n 的数组 answer 返回获奖，其中 answer[i] 是第 i 位运动员的获奖情况
+    public String[] findRelativeRanks(int[] score) {
+        String[] top3 = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        int n = score.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = score[i];
+            arr[i][1] = i;
+        }
+        Arrays.sort(arr, (x, y) -> y[0] - x[0]);
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            if (i < 3) {
+                res[arr[i][1]] = top3[i];
+            } else {
+                res[arr[i][1]] = Integer.toString(i + 1);
+            }
+        }
+        return res;
+    }
+
     //1446. 连续字符
     //给你一个字符串 s ，字符串的「能量」定义为：只包含一种字符的最长非空子字符串的长度。
     //请你返回字符串的能量。
