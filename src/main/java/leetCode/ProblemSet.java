767ca4094c306246e03827e35c65ded5a05d57d3
hui <fwu@today36524.com>
2021-12-21 09:24:04 +0800

    add 一年中的第几天（easy 模拟）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -117,6 +117,25 @@ public class ProblemSet {
         public Node child;
     }
 
+    //1154. 一年中的第几天
+//    给你一个字符串 date ，按 YYYY-MM-DD 格式表示一个 现行公元纪年法 日期。请你计算并返回该日期是当年的第几天。
+//
+//    通常情况下，我们认为 1 月 1 日是每年的第 1 天，1 月 2 日是每年的第 2 天，依此类推。每个月的天数与现行公元纪年法（格里高利历）一致。
+    public int dayOfYear(String date) {
+        String[] times = date.split("-");
+        int year = Integer.valueOf(times[0]);
+        int month = Integer.valueOf(times[1]);
+        int day = Integer.valueOf(times[2]);
+        int count[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            count[1]++;
+        }
+        int sum = 0;
+        for (int i = 0; i < month - 1; i++){
+            sum += count[i];
+        }
+        return sum + day;
+    }
     //1518. 换酒问题
     //小区便利店正在促销，用 numExchange 个空酒瓶可以兑换一瓶新酒。你购入了 numBottles 瓶酒。
     //如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
