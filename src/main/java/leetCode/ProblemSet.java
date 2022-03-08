8c5010fd9cb7b7dfda99bf57bc5d501c81e0e89a
hui <fwu@today36524.com>
2022-03-08 09:54:18 +0800

    add 蜡烛之间的盘子(mid 前缀和 + 预处理)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -139,7 +139,41 @@ public class ProblemSet {
         public Node next;
         public Node child;
     }
-
+    //2055. 蜡烛之间的盘子
+    //给你一个长桌子，桌子上盘子和蜡烛排成一列。给你一个下标从 0 开始的字符串 s ，它只包含字符 '*' 和 '|' ，其中 '*' 表示一个 盘子 ，'|' 表示一支 蜡烛 。
+    //同时给你一个下标从 0 开始的二维整数数组 queries ，其中 queries[i] = [lefti, righti] 表示 子字符串 s[lefti...righti] （包含左右端点的字符）。对于每个查询，你需要找到 子字符串中 在 两支蜡烛之间 的盘子的 数目 。如果一个盘子在 子字符串中 左边和右边 都 至少有一支蜡烛，那么这个盘子满足在 两支蜡烛之间 。
+    //比方说，s = "||**||**|*" ，查询 [3, 8] ，表示的是子字符串 "*||**|" 。子字符串中在两支蜡烛之间的盘子数目为 2 ，子字符串中右边两个盘子在它们左边和右边 都 至少有一支蜡烛。
+    //请你返回一个整数数组 answer ，其中 answer[i] 是第 i 个查询的答案。
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+        int n = s.length();
+        int[] preSum = new int[n];
+        for (int i = 0, sum = 0; i < n; i++) {
+            if(s.charAt(i) == '*'){
+                sum++;
+            }
+            preSum[i] = sum;
+        }
+        int[] left = new int[n];
+        for (int i = 0, l = -1; i < n; i++){
+            if(s.charAt(i) == '|'){
+                l = i;
+            }
+            left[i] = l;
+        }
+        int[] right = new int[n];
+        for(int i = n - 1, r = -1; i >= 0; i--){
+            if(s.charAt(i) == '|'){
+                r = i;
+            }
+            right[i] = r;
+        }
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++){
+            int x = right[queries[i][0]], y = left[queries[i][1]];
+            res[i] = x == -1 || y == -1 || x >= y ? 0 : preSum[y] - preSum[x];
+        }
+        return res;
+    }
 //    504. 七进制数
     //给定一个整数 num，将其转化为 7 进制，并以字符串形式输出。
     public String convertToBase7(int num) {
