fb9fb20a1f8bcb827aedaea9f15bd42245266528
hui <fwu@today36524.com>
2022-03-09 15:48:42 +0800

    add 得分最高的最小轮调(hard 差分数组)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -111,7 +111,8 @@ public class ProblemSet {
 //        System.out.println(maximumRequests(5, a));
 //        System.out.println(convert("PAYPALISHIRING", 3));
 //        System.out.println(nearestPalindromic("12345"));
-        System.out.println(subArrayRanges(new int[]{1,2,3}));
+//        System.out.println(subArrayRanges(new int[]{1,2,3}));
+        System.out.println(bestRotation(new int[]{2,3,1,4,0}));
     }
 
     public static class TreeNode {
@@ -138,6 +139,34 @@ public class ProblemSet {
         public Node prev;
         public Node next;
         public Node child;
+    }
+    //798. 得分最高的最小轮调
+    //给你一个数组 nums，我们可以将它按一个非负整数 k 进行轮调，这样可以使数组变为 [nums[k], nums[k + 1], ... nums[nums.length - 1], nums[0], nums[1], ..., nums[k-1]] 的形式。此后，任何值小于或等于其索引的项都可以记作一分。
+    //例如，数组为 nums = [2,4,1,3,0]，我们按 k = 2 进行轮调后，它将变成 [1,3,0,2,4]。这将记为 3 分，因为 1 > 0 [不计分]、3 > 1 [不计分]、0 <= 2 [计 1 分]、2 <= 3 [计 1 分]，4 <= 4 [计 1 分]。
+    //在所有可能的轮调中，返回我们所能得到的最高分数对应的轮调下标 k 。如果有多个答案，返回满足条件的最小的下标 k 。
+    public static int bestRotation(int[] nums) {
+        int n = nums.length;
+        int[] diffs = new int[n];
+        for (int i = 0; i < n; i++) {
+            int low = (i + 1) % n;
+            int high = (i - nums[i] + n + 1) % n;
+            diffs[low]++;
+            diffs[high]--;
+            if (low >= high) {
+                diffs[0]++;
+            }
+        }
+        int bestIndex = 0;
+        int maxScore = 0;
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            score += diffs[i];
+            if (score > maxScore) {
+                bestIndex = i;
+                maxScore = score;
+            }
+        }
+        return bestIndex;
     }
     //2055. 蜡烛之间的盘子
     //给你一个长桌子，桌子上盘子和蜡烛排成一列。给你一个下标从 0 开始的字符串 s ，它只包含字符 '*' 和 '|' ，其中 '*' 表示一个 盘子 ，'|' 表示一支 蜡烛 。
