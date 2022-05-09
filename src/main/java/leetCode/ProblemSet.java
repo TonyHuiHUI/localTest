66e2717ca31d69cd12697546912ef4a3b28a0ac6
hui <fwu@today36524.com>
2022-05-09 15:44:06 +0800

    add 增减字符串匹配(easy 贪心)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -144,6 +144,22 @@ public class ProblemSet {
         public Node child;
     }
 
+    //942. 增减字符串匹配
+    //由范围 [0,n] 内所有整数组成的 n + 1 个整数的排列序列可以表示为长度为 n 的字符串 s ，其中:
+    //    如果 perm[i] < perm[i + 1] ，那么 s[i] == 'I'
+    //    如果 perm[i] > perm[i + 1] ，那么 s[i] == 'D'
+    //给定一个字符串 s ，重构排列 perm 并返回它。如果有多个有效排列perm，则返回其中 任何一个 。
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int left = 0, right = n;
+        int[] result = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            result[i] = s.charAt(i) == 'I' ? left++ : right--;
+        }
+        result[n] = left;
+        return result;
+    }
+
     //433. 最小基因变化
     //基因序列可以表示为一条由 8 个字符组成的字符串，其中每个字符都是 'A'、'C'、'G' 和 'T' 之一。
     //假设我们需要调查从基因序列 start 变为 end 所发生的基因变化。一次基因变化就意味着这个基因序列中的一个字符发生了变化。
@@ -178,8 +194,8 @@ public class ProblemSet {
                             StringBuilder sb = new StringBuilder(cur);
                             sb.setCharAt(j, keys[k]);
                             String next = sb.toString();
-                            if(!visited.contains(next) && bankSet.contains(next)){
-                                if (next.equals(end)){
+                            if (!visited.contains(next) && bankSet.contains(next)) {
+                                if (next.equals(end)) {
                                     return step;
                                 }
                                 queue.offer(next);
