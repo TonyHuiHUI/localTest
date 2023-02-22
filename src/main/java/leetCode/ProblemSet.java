685bf217b60880d14fc1b8b570dbf9af465e08db
TonyHuiHUI <wufeizz@qq.com>
2023-02-22 14:18:00 +0800

    add 石子游戏II（mid dp）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -179,6 +179,33 @@ public class ProblemSet {
         public Node child;
     }
 
+//    1140. 石子游戏 II
+//    爱丽丝和鲍勃继续他们的石子游戏。许多堆石子 排成一行，每堆都有正整数颗石子 piles[i]。游戏以谁手中的石子最多来决出胜负。
+//
+//    爱丽丝和鲍勃轮流进行，爱丽丝先开始。最初，M = 1。
+//
+//    在每个玩家的回合中，该玩家可以拿走剩下的 前 X 堆的所有石子，其中 1 <= X <= 2M。然后，令 M = max(M, X)。
+//
+//    游戏一直持续到所有石子都被拿走。
+//
+//    假设爱丽丝和鲍勃都发挥出最佳水平，返回爱丽丝可以得到的最大数量的石头。
+    public int stoneGameII(int[] piles) {
+        int len = piles.length, sum = 0;
+        int[][] dp = new int[len][len + 1];
+        for (int i = len - 1; i >= 0; i--){
+            sum += piles[i];
+            for (int M = 1; M <= len; M++){
+                if (i + 2 * M >= len){
+                    dp[i][M] = sum;
+                }else {
+                    for (int j = 1; j <= 2 * M; j++){
+                        dp[i][M] = Math.max(dp[i][M], sum - dp[i + j][Math.max(M,j)]);
+                    }
+                }
+            }
+        }
+        return dp[0][1];
+    }
 
 //    2347. 最好的扑克手牌
 //    给你一个整数数组 ranks 和一个字符数组 suit 。你有 5 张扑克牌，第 i 张牌大小为 ranks[i] ，花色为 suits[i] 。
