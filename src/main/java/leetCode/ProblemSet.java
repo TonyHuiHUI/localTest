2622379276872b5ea54d3e949e8996a371cfb616
TonyHuiHUI <wufeizz@qq.com>
2023-03-22 11:36:23 +0800

    add 无矛盾的最佳球队(mid dp)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -178,6 +178,37 @@ public class ProblemSet {
         public Node next;
         public Node child;
     }
+
+
+//    1626. 无矛盾的最佳球队
+//    假设你是球队的经理。对于即将到来的锦标赛，你想组合一支总体得分最高的球队。球队的得分是球队中所有球员的分数 总和 。
+//
+//    然而，球队中的矛盾会限制球员的发挥，所以必须选出一支 没有矛盾 的球队。如果一名年龄较小球员的分数 严格大于 一名年龄较大的球员，则存在矛盾。同龄球员之间不会发生矛盾。
+//
+//    给你两个列表 scores 和 ages，其中每组 scores[i] 和 ages[i] 表示第 i 名球员的分数和年龄。请你返回 所有可能的无矛盾球队中得分最高那支的分数 。
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n = scores.length;
+        int res = 0;
+        int[][] player = new int[n][2];
+        for (int i = 0; i< n; i++){
+            player[i] = new int[]{scores[i], ages[i]};
+        }
+        Arrays.sort(player, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++){
+            for (int j = i - 1; j >= 0; j--){
+                if (player[j][1] <= player[i][1]){
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += player[i][0];
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+
+
     //2389. 和有限的最长子序列
 /*    给你一个长度为 n 的整数数组 nums ，和一个长度为 m 的整数数组 queries 。
 
