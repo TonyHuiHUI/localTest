820c26472067f27f00f06eddeefec921a8b70cf7
fwu <fwu@today36524.com.cn>
2024-04-08 14:15:46 +0800

    add (mid 递归)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -190,6 +190,39 @@ public class ProblemSet {
         public Node child;
     }
 
+
+
+/*    2009. 使数组连续的最少操作数
+    给你一个整数数组 nums 。每一次操作中，你可以将 nums 中 任意 一个元素替换成 任意 整数。
+
+    如果 nums 满足以下条件，那么它是 连续的 ：
+
+    nums 中所有元素都是 互不相同 的。
+    nums 中 最大 元素与 最小 元素的差等于 nums.length - 1 。
+    比方说，nums = [4, 2, 5, 3] 是 连续的 ，但是 nums = [1, 2, 3, 5, 6] 不是连续的 。
+
+    请你返回使 nums 连续 的 最少 操作次数。*/
+    public static int minOperations(int[] nums) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+        List<Integer> sortNums = new ArrayList<>(set);
+        Collections.sort(sortNums);
+        int res = n;
+        int j = 0;
+        for (int i = 0; i < sortNums.size();i++){
+            int left = sortNums.get(i);
+            int right = left + n - 1;
+            while (j < sortNums.size() && sortNums.get(j) <= right){
+                res = Math.min(res, n - (j - i + 1));
+                j++;
+            }
+        }
+        return  res;
+    }
+
 /*    894. 所有可能的真二叉树
     给你一个整数 n ，请你找出所有可能含 n 个节点的 真二叉树 ，并以列表形式返回。答案中每棵树的每个节点都必须符合 Node.val == 0 。
     答案的每个元素都是一棵真二叉树的根节点。你可以按 任意顺序 返回最终的真二叉树列表。
