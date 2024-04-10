66af9a4ffd0437ba36d541aa98fb378bb372c573
fwu <fwu@today36524.com.cn>
2024-04-10 11:22:49 +0800

    add 修改后的最大二进制字符串（mid 贪心）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -160,7 +160,7 @@ public class ProblemSet {
 //        System.out.println(str2.intern() == str2);
 
 //        System.out.println(queryString("0110", 3));
-        System.out.println(allPossibleFBT(7));
+        System.out.println(maximumBinaryString("000110"));
     }
 
 
@@ -188,6 +188,38 @@ public class ProblemSet {
         public Node prev;
         public Node next;
         public Node child;
+    }
+
+
+
+    /*
+    1702. 修改后的最大二进制字符串
+    给你一个二进制字符串 binary ，它仅有 0 或者 1 组成。你可以使用下面的操作任意次对它进行修改：
+
+操作 1 ：如果二进制串包含子字符串 "00" ，你可以用 "10" 将其替换。
+比方说， "00010" -> "10010"
+操作 2 ：如果二进制串包含子字符串 "10" ，你可以用 "01" 将其替换。
+比方说， "00010" -> "00001"
+请你返回执行上述操作任意次以后能得到的 最大二进制字符串 。如果二进制字符串 x 对应的十进制数字大于二进制字符串 y 对应的十进制数字，那么我们称二进制字符串 x 大于二进制字符串 y 。
+     */
+
+    public static String maximumBinaryString(String binary) {
+        int n = binary.length();
+        char[] b = binary.toCharArray();
+        int j = 0;
+        for (int i = 0; i < n; i++){
+            if(b[i] == '0'){
+                while(j <= i || (j < n && b[j] == '1')){
+                    j++;
+                }
+                if (j < n){
+                    b[j] = '1';
+                    b[i] = '1';
+                    b[i+1] = '0';
+                }
+            }
+        }
+        return  new String(b);
     }
 
 
