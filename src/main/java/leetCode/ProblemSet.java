bcbba1c19faf1ab99fafc9b4531d07b78bc2f998
fwu <fwu@today36524.com.cn>
2024-04-18 11:19:07 +0800

    add 从双倍数组中还原原数组(mid 排序 hash)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -197,6 +197,36 @@ public class ProblemSet {
         public Node child;
     }
 
+/*    2007. 从双倍数组中还原原数组
+    一个整数数组 original 可以转变成一个 双倍 数组 changed ，转变方式为将 original 中每个元素 值乘以 2 加入数组中，然后将所有元素 随机打乱 。
+
+    给你一个数组 changed ，如果 change 是 双倍 数组，那么请你返回 original数组，否则请返回空数组。original 的元素可以以 任意 顺序返回。*/
+    public int[] findOriginalArray(int[] changed) {
+        int n = changed.length;
+        if (n % 2 == 1) {
+            return new int[0];
+        }
+        int[] res = new int[ n / 2];
+        Arrays.sort(changed);
+        HashMap<Integer, Integer> count =  new HashMap<>();
+        for (int a : changed){
+            count.put(a, count.getOrDefault(a,0) + 1);
+        }
+        int idx = 0;
+        for (int a : changed){
+            if (count.get(a) == 0){
+                continue;
+            }
+            count.put(a,count.get(a) - 1);
+            if(count.getOrDefault(a * 2, 0) == 0){
+                return new int[0];
+            }
+            count.put(a * 2, count.get(a * 2) - 1);
+            res[idx++] = a;
+        }
+        return res;
+    }
+
 
 
 /*
