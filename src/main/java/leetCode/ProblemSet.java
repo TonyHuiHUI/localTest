772a53b60708773f6c666af07177ad5c1be87743
fwu <fwu@today36524.com.cn>
2024-05-16 17:08:26 +0800

    add 你可以工作的最大周数(mid 贪心)

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -203,6 +203,35 @@ public class ProblemSet {
         public Node child;
     }
 
+//    1953. 你可以工作的最大周数
+//    给你 n 个项目，编号从 0 到 n - 1 。同时给你一个整数数组 milestones ，其中每个 milestones[i] 表示第 i 个项目中的阶段任务数量。
+//
+//    你可以按下面两个规则参与项目中的工作：
+//
+//    每周，你将会完成 某一个 项目中的 恰好一个 阶段任务。你每周都 必须 工作。
+//    在 连续的 两周中，你 不能 参与并完成同一个项目中的两个阶段任务。
+//    一旦所有项目中的全部阶段任务都完成，或者仅剩余一个阶段任务都会导致你违反上面的规则，那么你将 停止工作 。注意，由于这些条件的限制，你可能无法完成所有阶段任务。
+//
+//    返回在不违反上面规则的情况下你 最多 能工作多少周。
+    public long numberOfWeeks(int[] milestones) {
+        int longest = milestones[0]; // 耗时最长工作所需周数
+        long rest = 0;
+        for (int count : milestones) {
+            longest = Math.max(longest, count);
+            rest += count;
+        }
+        // 其余工作共计所需周数
+        rest -= longest;
+        if (longest > rest + 1) {
+            // 此时无法完成所耗时最长的工作
+            return rest * 2 + 1;
+        } else {
+            // 此时可以完成所有工作
+            return longest + rest;
+        }
+    }
+
+
 /*    2589. 完成所有任务的最少时间
     你有一台电脑，它可以 同时 运行无数个任务。给你一个二维整数数组 tasks ，其中 tasks[i] = [starti, endi, durationi] 表示第 i 个任务需要在 闭区间 时间段 [starti, endi] 内运行 durationi 个整数时间点（但不需要连续）。
 
