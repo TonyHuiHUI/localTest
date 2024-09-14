be1438d9164cbabe3890ce17ce96a5c2991b9920
fwu <fwu@today36524.com.cn>
2024-09-14 17:08:22 +0800

    旋转图像（mid）

--- src/main/java/leetCode/ProblemSet.java
+++ src/main/java/leetCode/ProblemSet.java
@@ -205,6 +205,49 @@ public class ProblemSet {
         System.out.println(fourSum(a,1000000000));
     }
 
+    public void rotate(int[][] matrix) {
+        //临时矩阵
+//        int n = matrix.length;
+//        int[][] temp = new int[n][n];
+//        for (int i = 0 ; i < n; i++){
+//            for (int j = 0; j < n; j++){
+//                temp[j][n - i - 1] = matrix[i][j];
+//            }
+//        }
+//        for (int i = 0 ; i < n; i++){
+//            for (int j = 0; j < n; j++){
+//                matrix[i][j] = temp[i][j];
+//            }
+//        }
+//        //原地转换
+//        int n = matrix.length;
+//        for (int i = 0; i < n / 2; i++){
+//            for (int j = 0; j < (n + 1) / 2; j++){
+//                int tmp = matrix[i][j];
+//                matrix[i][j] = matrix[n - j - 1][i];
+//                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+//                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+//                matrix[j][n - i - 1] = tmp;
+//            }
+//        }
+        //先转置再左右翻转
+        int n = matrix.length;
+        for (int i = 0; i < n; i++){
+            for (int j = i + 1; j < n; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n / 2; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j]  = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
 
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
